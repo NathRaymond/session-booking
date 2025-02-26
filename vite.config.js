370cf88f8507dfb15ec5f5b2ecd53134a3930955
Adebayo Nathaniel <adebayonathaniel79@gmail.com>
2025-02-26 12:15:17 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build', // Ensure this matches your deployment folder
         }),
     ],
+    build: {
+        manifest: true,       // Ensure the manifest is generated
+        outDir: 'public/build', // Output directory for your assets
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+            },
+        },
+    },
 });
+
