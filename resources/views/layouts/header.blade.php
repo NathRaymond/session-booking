2a556a4ab751d3baaf23becea35718773c54e412
Adebayo Nathaniel <adebayonathaniel79@gmail.com>
2025-02-26 11:03:44 +0100

    updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
         <ul>
             <li><a href="/" class="active" style="font-size: 17px; color: 	#800020 !important;">Home</a></li>
             <div class="col-lg-8">
-                <a href="#" class="btn-get-started" style="height: 40px; width: 100px; border-radius: 25px; border: 2px solid white; display: flex; align-items: center; justify-content: center; text-decoration: none; color: white;">ENTER</a>
+                <a href="#" class="btn-get-started" style="height: 40px; width: 100px; border-radius: 25px; border: 2px solid white; display: flex; align-items: center; justify-content: center; text-decoration: none; color: #800020 !important;">ENTER</a>
 
             </div>
         </ul>
